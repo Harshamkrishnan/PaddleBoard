fcf0ed78523a41cf87234e905dce4678af69a839
Harsha Mk <harshamkrishnan@gmail.com>
2022-05-19 14:44:43 +0530

    moving the paddle

--- paddleboard.js
+++ paddleboard.js
@@ -24,3 +24,46 @@ function welcome() {
     document.getElementById("demo").innerHTML = "Welcome " + name;
 
 }
+
+let paddle1 = document.getElementById('paddle1');
+/*let paddle2 = document.getElementsByClassName('.paddle2');*/
+let moveBy = 100;
+
+// window.addEventListener('load', () =>{
+//     paddle1.style.position = 'absolute';
+//     paddle1.style.left = 0;
+//     paddle1.style.top = 0;
+//     /*paddle2.position = 'absolute';
+//     paddle2.right = 0;
+//     paddle2.top = 0;*/
+// });
+
+window.addEventListener('keyup', (e) =>{
+    console.log(`event called ${e.key}`);
+    console.log(paddle1.style.left);
+    let newPos = 0;
+    switch(e.key){
+        case 'ArrowLeft' :
+            newPos = parseInt(paddle1.style.left) - moveBy;
+            console.log(`Arrow left called ${newPos}`);
+            paddle1.style.left = newPos;
+            break;
+        case 'ArrowRight' :
+            newPos = parseInt(paddle1.style.left) + moveBy;
+            console.log(`Arrow right called ${newPos}`);
+            paddle1.style.left = newPos;
+            break;
+        case 'ArrowUp' :
+            newPos = parseInt(paddle1.style.top) + moveBy;
+            console.log(`Arrow up called ${newPos}`);
+            paddle1.style.left = newPos;
+            break;
+        case 'ArrowDown' :
+            newPos = parseInt(paddle1.style.top) - moveBy;
+            console.log(`Arrow down called ${newPos}`);
+            paddle1.style.left = newPos;
+            break;
+        case 'Default' :
+            break;
+    }
+})
